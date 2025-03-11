c2c074a2e89748b2caab6ce7e671f0f87afd1e38
beemovier <erick129ferreira@gmail.com>
2025-03-10 22:30:47 -0300

    polinizando

--- app.js
+++ app.js
@@ -6,16 +6,28 @@ alert('QUER CAFÉ?');
 alert('TA COM FOME?');
 alert('QUEM NÃO TÁ?');
 
-//parte botão
-const switcher = document.querySelector('.btn');
-switcher.addEventListener('click', function(){
-    document.body.classList.toggle('dark-theme')
+//isso vai impedir que o script rode antes de carregar o botão
+document.addEventListener('DOMContentLoaded', function () {
 
-    var className = document.body.className;
-    if(className == "light-theme"){
-        this.textContent = "Dark";
-    }
-    else {this.textContent = "Light";}
-    console.log('current class name: ' + className);
+    //parte botão
+    const switcher = document.querySelector('.btn');
+    switcher.addEventListener('click', function () {
+
+        //Adciona a lasse 'light-theme' ao body inicialmente
+        document.body.classList.add('light-theme');
+
+        //alterna entre os temas
+        document.body.classList.toggle('dark-theme');
+        // document.body.classList.toggle('light-theme');
+
+        var className = document.body.className;
+        if (className == "light-theme") {
+            this.textContent = "Dark";
+        }
+        else { this.textContent = "Light"; }
+        console.log('current class name: ' + className);
+    });
+    //javascript costumamos usar letras concatenadas para nomes de variáveis com mais de uma palavra className
 });
-//javascript costumamos usar letras concatenadas para nomes de variáveis com mais de uma palavra className
+//trocar repo: git remot set-url origin https...
+//novo repo: git remote add origin https...
